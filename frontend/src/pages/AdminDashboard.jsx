815ca5ff037a5a902697916e84d5e2c1e73ffd96
juvanfrank <juvanfk@gmail.com>
2026-03-10 14:41:10 +0530

    Added frontend folder to repository

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,14 @@
+function AdminDashboard() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-orange-600">
+        Placement Coordinator Dashboard 📊
+      </h1>
+      <p className="mt-4 text-gray-600">
+        Welcome to the Placement Panel
+      </p>
+    </div>
+  );
+}
+
+export default AdminDashboard;
